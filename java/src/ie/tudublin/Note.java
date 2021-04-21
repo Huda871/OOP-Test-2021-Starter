5f0c6ee9e45c49951619a43730b057a39e7f9e18
= <c19738789@mytudublin.ie>
2021-04-21 18:23:19 +0100

    Note

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,45 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Note {
+    private char note;
+    private int duration;
+
+    public char getNote() {
+        return note;
+    }
+    public void setNote(char note) {
+        this.note = note;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Note(char note, int duration) {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    /*public Note(TableRow r)
+    {
+        this(r.get("Note"), r.getInt("duration"));
+    }*/
+    
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + "]";
+    }
+
+    //ArrayList<Note>note = new ArrayList<Note>();
+    
+
+
+    
+    
+}
+
+
